295a414b80c3d988fb4ce172d9263008a669d895
Benjoow <benjamin.wojeez@gmail.com>
2021-08-31 15:56:37 +0200

    INSERT map.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/map',
+    name: 'Map',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Map.vue')
   }
 ]
 
---
+++ src/views/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <div id="timeline">
+      <div class="pointer"></div>
+      <div class="pointer"></div>
+      <div class="pointer"></div>
+    </div>
+    <div id="map"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.createMap();
+  },
+
+  methods: {
+    createMap() {
+      const map = L.map('map').setView([49.762085, 4.726096], 13)
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZGR0LWFyZGVubmVzIiwiYSI6ImNrc3Qxb29xeDA5ZXkyb3BjOHI1bWZuMXYifQ.rpxnQ4xjiaFRnIFM18eP7A'
+      }).addTo(map);
+      const marker = L.marker([49.762085, 4.726096]).addTo(map);
+    }
+  }
+}
+</script>

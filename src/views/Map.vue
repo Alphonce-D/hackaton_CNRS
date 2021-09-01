5dec09b5b5aa8c09f2e2b872e9e4eaa356a12e38
Benjoow <benjamin.wojeez@gmail.com>
2021-09-01 09:19:16 +0200

    Update marker map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,33 +1,95 @@
 <template>
   <section>
     <div id="timeline">
-      <div class="pointer"></div>
-      <div class="pointer"></div>
-      <div class="pointer"></div>
+      <div class="periode">
+        <div class="pointer" @click="antiMarker(this.gisements)"></div>
+        <p>Antiquité</p>
+      </div>
+      <div class="periode">
+        <div class="pointer" @click="modernMarker(this.gisements)"></div>
+        <p>Epoque Moderne</p>
+      </div>
+      <div class="periode">
+        <div class="pointer" @click="romainMarker(this.gisements)"></div>
+        <p>Epoque Romaine</p>
+      </div>
     </div>
     <div id="map"></div>
   </section>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
-  mounted() {
-    this.createMap();
+  data: () => ({
+    gisements: null,
+    layerGroup: null,
+  }),
+
+  async mounted() {
+    const response = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/coordinates',
+    })
+    this.gisements = response.data;
+    this.createMap(this.gisements);
   },
 
   methods: {
-    createMap() {
-      const map = L.map('map').setView([49.762085, 4.726096], 13)
+    
+
+    createMap(coords) {
+      const map = L.map('map').setView([49.1182, 0.9582], 13)
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        maxZoom: 10,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoiZGR0LWFyZGVubmVzIiwiYSI6ImNrc3Qxb29xeDA5ZXkyb3BjOHI1bWZuMXYifQ.rpxnQ4xjiaFRnIFM18eP7A'
-      }).addTo(map);
-      const marker = L.marker([49.762085, 4.726096]).addTo(map);
+        }).addTo(map);
+        this.layerGroup = L.layerGroup().addTo(map);
+      // coords.forEach(item => {
+      //   L.marker( [item.x, item.y] ).addTo(map);
+      // });
+      for(let i = 0; i < 20; i++) {
+        const marker = L.marker( [coords[i].x, coords[i].y] ).addTo(this.layerGroup);
+        marker.on('click', this.redirect);
+      }
+
+    },
+
+
+  antiMarker(coords) {
+    this.layerGroup.clearLayers();
+     for(let i = 0; i < 20; i++) {
+      const marker = L.marker( [coords[i].x, coords[i].y]).addTo(this.layerGroup);
+      marker.on('click', this.redirect);
     }
+  },
+
+  modernMarker(coords) {
+    this.layerGroup.clearLayers();
+    for(let i = 21; i < 47; i++) {
+      const marker = L.marker( [coords[i].x, coords[i].y]).addTo(this.layerGroup);
+      marker.on('click', this.redirect);
+    }
+  },
+
+  romainMarker(coords) {
+    this.layerGroup.clearLayers();
+    for( let i = 48; i < 59; i++) {
+      const marker = L.marker( [coords[i].x, coords[i].y] ).addTo(this.layerGroup);
+      marker.on('click', this.redirect);
+    }
+  },
+
+  redirect() {
+    this.$router.push('/');
+  }
+    
   }
 }
 </script>
